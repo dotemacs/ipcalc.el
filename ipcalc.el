f4ac17eb8d4cb311b1419d89fe745e23881a19e2
Александар Симић <asimic@gmail.com>
2016-09-27 10:01:22 +0100

    Style change prior to MELPA acceptance
    Conforiming to Emacs Lisp style guide.
    
    All the functions that had 'ipcalc/' prefix have now been renamed to
    'ipcalc-'.
    
    The variable `ipcalc-cidr-default' has now been renamed to
    `ipcalc--cidr-default'.

--- ipcalc.el
+++ ipcalc.el
@@ -46,9 +46,9 @@
 
 (require 'cl-lib)
 
-(defconst ipcalc-cidr-default 32 "CIDR value.")
+(defconst ipcalc--cidr-default 32 "CIDR value.")
 
-(defun ipcalc/int-to-bin-string (n &optional length)
+(defun ipcalc-int-to-bin-string (n &optional length)
   ;; 08 Jun 1997 Jamie Zawinski <jwz@netscape.com> comp.emacs
   "Convert integer N to bit string (LENGTH, default 8)."
   (let* ((i  0)
@@ -60,32 +60,32 @@
       (setq i (1+ i)))
     s))
 
-(defun ipcalc/octets-as-binary (list-of-octets)
+(defun ipcalc-octets-as-binary (list-of-octets)
   "Return LIST-OF-OCTETS as a single binary string."
   (let ((binary ""))
   (while list-of-octets
     (setq binary
           (concat binary
-                  (ipcalc/int-to-bin-string
+                  (ipcalc-int-to-bin-string
                    (string-to-number (car list-of-octets)))))
       (setq list-of-octets (cdr list-of-octets)))
   binary))
 
-(defun ipcalc/ip-to-octets (ip)
+(defun ipcalc-ip-to-octets (ip)
   "Split IP address and return the octets."
   (let ((octets (split-string ip "\\.")))
     (if (= 4 (length octets))
         octets
       (message "not correct IP format"))))
 
-(defun ipcalc/ones-and-pad (num)
+(defun ipcalc-ones-and-pad (num)
   "Return 1's equal to NUM and pad the rest up to 32."
-  (unless (and (<= num ipcalc-cidr-default) (> num 0))
+  (unless (and (<= num ipcalc--cidr-default) (> num 0))
     (error "Wrong value provided"))
   (concat (make-string num ?1)
-          (make-string (- ipcalc-cidr-default num) ?0)))
+          (make-string (- ipcalc--cidr-default num) ?0)))
 
-(defun ipcalc/invert-binary (num)
+(defun ipcalc-invert-binary (num)
   "Invert NUM's 1s to 0s and vice versa.
 
   (credit: the simpler version below re-writen by pjb from #emacs
@@ -94,34 +94,34 @@
        (lambda (ch) (aref "10" (cl-position ch "01")))
        (cl-remove-if-not (lambda (ch) (cl-position ch "01")) num)))
 
-(defun ipcalc/network (ip cidr)
+(defun ipcalc-network (ip cidr)
   "Takes IP & CIDR and produces network."
   (let ((cidr-as-int (string-to-number cidr)))
     (concat
-     (substring (ipcalc/octets-as-binary
-                 (ipcalc/ip-to-octets ip)) 0 cidr-as-int)
-     (make-string (- ipcalc-cidr-default cidr-as-int) ?0))))
+     (substring (ipcalc-octets-as-binary
+                 (ipcalc-ip-to-octets ip)) 0 cidr-as-int)
+     (make-string (- ipcalc--cidr-default cidr-as-int) ?0))))
 
-(defun ipcalc/host+1 (binary-ip)
+(defun ipcalc-host+1 (binary-ip)
   "Increment the given BINARY-IP by 1."
   (let ((tmp binary-ip))
-    (aset tmp (- ipcalc-cidr-default 1) ?1)
+    (aset tmp (- ipcalc--cidr-default 1) ?1)
     tmp))
 
-(defun ipcalc/host-max (ip cidr)
+(defun ipcalc-host-max (ip cidr)
   "Given IP and CIDR, return maximum host as a binary value."
   (let ((count (string-to-number cidr))
-        (max (- ipcalc-cidr-default 1)))
+        (max (- ipcalc--cidr-default 1)))
     (while (< count max)
       (aset ip count ?1)
       (setq count(cl-incf count)))
     ip))
 
-(defun ipcalc/hosts/net (num)
+(defun ipcalc-hosts/net (num)
   "Calculate Hosts/Net for the NUM given."
-  (- (expt 2 (- ipcalc-cidr-default num)) 2))
+  (- (expt 2 (- ipcalc--cidr-default num)) 2))
 
-(defmacro list-to-string (lst)
+(defmacro ipcalc-list-to-string (lst)
   "Convert LST to string."
   `(mapconcat
     #'(lambda (val)
@@ -130,16 +130,16 @@
                    val)))
     ,lst ""))
 
-(defun ipcalc/insert-values (lst)
+(defun ipcalc-insert-values (lst)
   "Insert given LST."
   (insert
-   (list-to-string lst)))
+   (ipcalc-list-to-string lst)))
 
-(defun ipcalc/bin-to-int (bin)
+(defun ipcalc-bin-to-int (bin)
   "Convert binary value BIN to integer."
   (int-to-string (read (concat "#b" bin))))
 
-(defun ipcalc/binary-to-ip (binary)
+(defun ipcalc-binary-to-ip (binary)
   "Convert BINARY to IP address."
   (let* (full-ip
         (count 0)
@@ -148,7 +148,7 @@
         (3rd-octet (substring binary 16 24))
         (4th-octet (substring binary 24 32))
         (octets (mapcar
-                 'ipcalc/bin-to-int
+                 'ipcalc-bin-to-int
                  `(,1st-octet ,2nd-octet ,3rd-octet ,4th-octet))))
     (while (< count 3)
       (setq full-ip (concat full-ip (nth count octets) "."))
@@ -159,21 +159,21 @@
 (defun ipcalc (ip/cidr)
   "IP calculator for given IP/CIDR."
   (let* ((ip (car (split-string ip/cidr "/")))
-         (ip-in-binary (ipcalc/octets-as-binary (ipcalc/ip-to-octets ip)))
+         (ip-in-binary (ipcalc-octets-as-binary (ipcalc-ip-to-octets ip)))
          (cidr (car (cdr (split-string ip/cidr "/"))))
          (cidr-int (string-to-number cidr))
-         (cidr-binary (ipcalc/ones-and-pad cidr-int))
-         (wildcard-binary (ipcalc/invert-binary (ipcalc/ones-and-pad cidr-int)))
-         (wildcard-ip (ipcalc/binary-to-ip wildcard-binary))
-         (netmask (ipcalc/binary-to-ip (ipcalc/invert-binary wildcard-binary)))
-         (net-binary (ipcalc/network ip cidr))
-         (net-ip (ipcalc/binary-to-ip net-binary))
-         (host-max-binary (ipcalc/host-max (ipcalc/network ip cidr) cidr))
-         (host-max-ip (ipcalc/binary-to-ip host-max-binary))
-         (host-min-binary (ipcalc/host+1 (ipcalc/network ip cidr)))
-         (host-min-ip (ipcalc/binary-to-ip host-min-binary))
-         (broadcast-binary (ipcalc/host+1 (ipcalc/host-max net-binary cidr)))
-         (broadcast-ip (ipcalc/binary-to-ip broadcast-binary))
+         (cidr-binary (ipcalc-ones-and-pad cidr-int))
+         (wildcard-binary (ipcalc-invert-binary (ipcalc-ones-and-pad cidr-int)))
+         (wildcard-ip (ipcalc-binary-to-ip wildcard-binary))
+         (netmask (ipcalc-binary-to-ip (ipcalc-invert-binary wildcard-binary)))
+         (net-binary (ipcalc-network ip cidr))
+         (net-ip (ipcalc-binary-to-ip net-binary))
+         (host-max-binary (ipcalc-host-max (ipcalc-network ip cidr) cidr))
+         (host-max-ip (ipcalc-binary-to-ip host-max-binary))
+         (host-min-binary (ipcalc-host+1 (ipcalc-network ip cidr)))
+         (host-min-ip (ipcalc-binary-to-ip host-min-binary))
+         (broadcast-binary (ipcalc-host+1 (ipcalc-host-max net-binary cidr)))
+         (broadcast-ip (ipcalc-binary-to-ip broadcast-binary))
          (buffer "*ipcalc*"))
     (if (get-buffer buffer)
         (kill-buffer buffer))
@@ -182,11 +182,11 @@
      (format "Address:%15s%41s\n" ip ip-in-binary)
      (format "Netmask:%16s = %2s %34s\n" netmask cidr cidr-binary)
      (format "Wildcard:%11s%44s\n" wildcard-ip wildcard-binary)
-     (format "=>\nNetwork:%14s%42s\n" net-ip (ipcalc/network ip cidr))
+     (format "=>\nNetwork:%14s%42s\n" net-ip (ipcalc-network ip cidr))
      (format "HostMin:%14s%42s\n" host-min-ip host-min-binary)
      (format "HostMax:%16s%40s\n" host-max-ip host-max-binary)
      (format "Broadcast:%14s%40s\n" broadcast-ip broadcast-binary)
-     (format "Hosts/Net: %d\n" (ipcalc/hosts/net cidr-int)))))
+     (format "Hosts/Net: %d\n" (ipcalc-hosts/net cidr-int)))))
 
 (provide 'ipcalc)
 
